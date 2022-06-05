d121f8789355afd1d0207de1243d2d6de35da218
itsky123 <sky941129@gmail.com>
2022-06-05 19:26:53 +0800

    Tick for tips

--- pages/map/index.js
+++ pages/map/index.js
@@ -3,15 +3,21 @@ import {Status, Wrapper} from "@googlemaps/react-wrapper";
 import MapComponent from "./map";
 
 import data from '../../data/data.json'
-import {Col, Row, Container, Button, Input, Label} from "reactstrap";
+import {Col, Row, Container, Button, Input, Label, Modal, ModalFooter, ModalHeader, ModalBody} from "reactstrap";
+import {genItem} from "../../utils";
 
 
 const Map = () => {
     const apiKey = 'AIzaSyBeNAOLY6vPddJiTZ1_b0n-TaUdrM-mfMg'
 
+    const item = genItem()
+
     const [map, setMap] = useState(null);
     const [selectedType, setSelectedType] = useState('')
     const [currentLocation, setCurrentLocation] = useState(null)
+    const [checkBox, setCheckBox] = useState(false)
+    const [show, setShow] = useState(false);
+    const [modalText, setModalText] = useState('')
     const [currentLocationString, setCurrentLocationString] = useState('')
     const [markers, setMarkers] = useState([]);
     const [directionsRenderers, setDirectionsRenderers] = useState([])
@@ -21,61 +27,61 @@ const Map = () => {
 
     const iconBase = "/mapicon/";
     const s = [{
-        name: 'BEVERAGE CARTONS', file: 'BEVERAGE.png'
+        name: 'BEVERAGE CARTONS', type:'Tetra Pak' ,file: 'BEVERAGE.png'
     }, {
-        name: 'FLUORESCENT LAMPS AND TUBES', file: 'fluorescent.png'
+        name: 'FLUORESCENT LAMPS AND TUBES', type:'Fluorescent Lamps and Tubes', file: 'fluorescent.png'
     }, {
-        name: 'GLASS BOTTLES', file: 'GLASS.png'
+        name: 'GLASS BOTTLES',type:'Glass', file: 'GLASS.png'
     }, {
-        name: 'METALS', file: 'beam.png'
+        name: 'METALS', type: 'Metals',file: 'beam.png'
     }, {
-        name: 'PAPER', file: 'PPAPER.png'
+        name: 'Waste Paper',type: "Waste Paper", file: 'PAPER.png'
     }, {
-        name: 'PLASTICS', file: 'bag.png'
+        name: 'PLASTICS',type: "Plastics", file: 'bag.png'
     }, {
-        name: 'RECHARGEABLE BATTERIES', file: 'battery.png'
+        name: 'RECHARGEABLE BATTERIES',type: "Rechargeable Batteries", file: 'battery.png'
     }, {
-        name: 'REGULATED ELECTRICAL EQUIPMENT', file: 'appliances.png'
+        name: 'REGULATED ELECTRICAL EQUIPMENT',type: "Regulated Electrical Equipments (REE)", file: 'appliances.png'
     }, {
-        name: 'SMALL ELECTRICAL APPLIANCES', file: 'microwave.png'
+        name: 'SMALL ELECTRICAL APPLIANCES',type: "Small Electrical Appliances", file: 'microwave.png'
     }, {
-        name: 'COMPUTER PRODUCTS', file: 'computer.png'
+        name: 'COMPUTER PRODUCTS',type: "Regulated Electrical Equipments (REE)", file: 'computer.png'
     }, {
-        name: 'ELECTRICAL APPLIANCES', file: 'electricappliance.png'
+        name: 'ELECTRICAL APPLIANCES', type: "Regulated Electrical Equipments (REE)",file: 'electricappliance.png'
     }, {
-        name: 'OTHERS', file: 'others.png'
+        name: 'OTHERS',type: "Others", file: 'others.png'
     }, {
-        name: 'WASTE PLASTICS', file: 'plastic.png'
+        name: 'WASTE PLASTICS',type: "Plastics", file: 'plastic.png'
     }, {
-        name: 'METAL CANS', file: 'metal.png'
+        name: 'METAL CANS', type: 'Metals', file: 'metal.png'
     }, {
-        name: 'METAL CONTAINER', file: 'CONTAINER.png'
+        name: 'METAL CONTAINER', type: 'Metals', file: 'CONTAINER.png'
     }, {
-        name: 'PLASTIC BOTTLE', file: 'water.png'
+        name: 'PLASTIC BOTTLE',type: "Plastics", file: 'water.png'
     }, {
-        name: 'WASTE PAPER', file: 'paper.png'
+        name: 'WASTE PAPER',type: "Waste Paper", file: 'paper.png'
     }, {
-        name: 'ACCESSORIES', file: 'accessory.png'
+        name: 'ACCESSORIES',type: "Others", file: 'accessory.png'
     }, {
-        name: 'BOOKS', file: 'book.png'
+        name: 'BOOKS',type: "Waste Paper", file: 'book.png'
     }, {
-        name: 'CLOTHES', file: 'clothes.png'
+        name: 'CLOTHES',type: "Clothes", file: 'clothes.png'
     }, {
-        name: 'EDUCATIONAL TOYS', file: 'cubes.png'
+        name: 'EDUCATIONAL TOYS',type: "Others", file: 'cubes.png'
     }, {
-        name: 'STATIONERY', file: 'stationery.png'
+        name: 'STATIONERY',type: "Others", file: 'stationery.png'
     }, {
-        name: 'HOTEL SOAPS AND AMENITIES', file: 'toiletries.png'
+        name: 'HOTEL SOAPS AND AMENITIES',type: "Others", file: 'toiletries.png'
     }, {
-        name: 'SMALL WASTE ELECTRICAL AND ELECTRONIC EQUIPMENT', file: 'motor.png'
+        name: 'SMALL WASTE ELECTRICAL AND ELECTRONIC EQUIPMENT',type: "Small Electrical Appliances", file: 'motor.png'
     }, {
-        name: 'FOOD', file: 'hamburger.png'
+        name: 'FOOD', type: "Food",file: 'hamburger.png'
     }, {
-        name: 'FURNITURE (HOUSEHOLD)', file: 'couch.png'
+        name: 'FURNITURE (HOUSEHOLD)',type: "Furniture", file: 'couch.png'
     }, {
-        name: 'FURNITURE (INSTITUTION/COMMERCIAL)', file: 'desk.png'
-    },{
-        name: 'Waste Separation Bin', file: 'bin.png'
+        name: 'FURNITURE (INSTITUTION/COMMERCIAL)',type: "Furniture", file: 'desk.png'
+    }, {
+        name: 'Waste Separation Bin',type: "Waste Paper", file: 'bin.png'
     }]
 
     useEffect(() => {
@@ -91,6 +97,7 @@ const Map = () => {
     }, [selectedType, currentLocationString])
 
 
+    const handleClose = () => setShow(false);
 
     const render = (status) => {
         if (status === Status.LOADING) return <h3>{status} ..</h3>;
@@ -217,41 +224,82 @@ const Map = () => {
         setCircles(circles2)
     }
 
+    const handleSelectChange = (type) => {
+        setSelectedType(type)
+        console.log('6', checkBox, type, show)
+        if (checkBox) {
+            const target1 = s.find(i => i.name === type);
+            console.log(item)
+            const target = item.find(o => o.type === target1.type);
+            console.log(target);
+            setModalText(target?.guideline)
+            setShow(true)
+        }
+        console.log('j', checkBox, type, show)
+    }
+
+    const toggle = () => {
+        setShow(!show);
+    }
 
     return (
-        <Container className={'page-container'}>
-            <Row>
-                <Col xl={4} sm={4} lg={4}>
-                    <p>Choose Types:</p>
-                    <select onChange={e => setSelectedType(e.target.value)}>
-                        <option key='999' value={''}/>
-                        {generateSelect()}
-                    </select>
-                    <p>Your Current Location (Higher Priority): {JSON.stringify(currentLocation)}</p>
-                    <Label htmlFor={'currentLocationString'}>Input your location: </Label>
-                    <Input name={'currentLocationString'} type={'text'}
-                           onChange={e => setCurrentLocationString(e.target.value)}
-                           value={currentLocationString}
-                    />
-                    <br/>
-                    <br/>
-                    <Label htmlFor={'radius'}>Input Radius (metres): </Label>
-                    <Input name={'radius'} type={'text'} onChange={e => setRadius(e.target.value)}
-                           value={radius}/>
-                    <Button className={'btn-success'} onClick={() => drawCircle()}>Draw Circle</Button>
-                    <br/>
-                    <br/>
-                    <p>Total Distance (km): {totalDistance}</p>
-                    <br/>
-                    <Button className={'btn-danger'} onClick={() => clear()}>Clear</Button>
-                    <br/>
-                    <br/>
-                </Col>
-                <Col xl={8} sm={8} lg={8}>
-                    <Wrapper apiKey={apiKey} render={render}/>
-                </Col>
-            </Row>
-        </Container>)
+        <>
+            <Modal
+                isOpen={show}
+                toggle={toggle}
+            >
+                <ModalHeader toggle={toggle}>
+                    {'Recycling Tips'}
+                </ModalHeader>
+                <ModalBody>
+                    {modalText}
+                </ModalBody>
+                <ModalFooter>
+                    <Button
+                        color="success"
+                        onClick={toggle}
+                    >{'OK'}
+                    </Button>
+                </ModalFooter>
+            </Modal>
+            <Container className={'page-container'}>
+                <Row>
+                    <Col xl={4} sm={4} lg={4}>
+                        <p>Choose Types:</p>
+                        <select onChange={e => handleSelectChange(e.target.value)}>
+                            <option key='999' value={''}/>
+                            {generateSelect()}
+                        </select>
+                        <Label htmlFor={'checkBox'}>Tick for tips: </Label>
+                        <Input name={'checkBox'} type={'checkbox'} value={checkBox}
+                               onChange={() => setCheckBox(!checkBox)}/>
+                        <p>Your Current Location (Higher Priority): {JSON.stringify(currentLocation)}</p>
+                        <Label htmlFor={'currentLocationString'}>Input your location: </Label>
+                        <Input name={'currentLocationString'} type={'text'}
+                               onChange={e => setCurrentLocationString(e.target.value)}
+                               value={currentLocationString}
+                        />
+                        <br/>
+                        <br/>
+                        <Label htmlFor={'radius'}>Input Radius (metres): </Label>
+                        <Input name={'radius'} type={'text'} onChange={e => setRadius(e.target.value)}
+                               value={radius}/>
+                        <Button className={'btn-success'} onClick={() => drawCircle()}>Draw Circle</Button>
+                        <br/>
+                        <br/>
+                        <p>Total Distance (km): {totalDistance}</p>
+                        <br/>
+                        <Button className={'btn-danger'} onClick={() => clear()}>Clear</Button>
+                        <br/>
+                        <br/>
+                    </Col>
+                    <Col xl={8} sm={8} lg={8}>
+                        <Wrapper apiKey={apiKey} render={render}/>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
 
 }
 export default Map
--- pages/type-page/index.js
+++ pages/type-page/index.js
@@ -1,136 +1,10 @@
-import {useEffect, useState} from 'react'
-import {Status, Wrapper} from "@googlemaps/react-wrapper";
-import MapComponent from "../map";
-
-
-import {Col, Row, Container, Button, Input, Label, Table} from "reactstrap";
+import {Table} from "reactstrap";
 import styles from "./typePage.module.css"
+import {genItem} from "../../utils";
 
 const TypePage = () => {
 
-    const items = [
-        {
-            type: "Waste Paper",
-            img: ["/typeimage/news.jpg", "/typeimage/paper.jpg", "/typeimage/box.jpg"],
-            guideline:
-                <ol>
-                    <li>Remove plastic coated pages, plastic tape and other non-paper materials</li>
-                    <li>Keep the waste paper dry</li>
-                </ol>
-        }, {
-            type: "Metals",
-            img: ["/typeimage/metal.jpg","/typeimage/can.jpg"],
-            guideline:
-                <ol>
-                    <li>Empty metal cans for easy cleaning </li>
-                    <li>Queeze the aluminum beverage can as flat as possible</li>
-                    <li>Place properly disposed metal cans in metal recycling bins</li>
-                </ol>
-        }, {
-            type: "Plastics",
-            img: ["/typeimage/bag.jpg","/typeimage/pbottles.jpg","/typeimage/Yakult.jpg","/typeimage/shampoo.jpg"],
-            guideline:
-                <ol>
-                    <li>Remove the bottle cap first</li>
-                    <li>Pour out the liquid in the plastic bottle and do a simple cleaning</li>
-                    <li>Remove the poster</li>
-                    <li>Sort and recycle the plastic bottle caps, paper and bottle bodies and put them in the plastic recycling bin</li>
-                </ol>
-        }, {
-            type: "Glass",
-            img: ["/typeimage/bottles.jpg","/typeimage/glabot.jpg"],
-            guideline:
-                <ol>
-                    <li>Remove the caps.</li>
-                    <li>Rinse the containers before recycling.</li>
-                </ol>
-        }, {
-            type: "Regulated Electrical Equipments (REE)",
-            img: ["/typeimage/mon.jpg","/typeimage/washing.jpg","/typeimage/air.jpg","/typeimage/printer.jpg"],
-            guideline:
-                <ol>
-                    <li>Request the seller to deliver the new item and remove the old item of the same class on the same day for no additional cost.</li>
-                </ol>
-        }, {
-            type: "Small Electrical Appliances",
-            img: ["/typeimage/rice.jpg","/typeimage/hair.jpg","/typeimage/camera.jpg"],
-            guideline:
-            <ol>
-                <li>Call the Government's recycling hotline (2676 8888).</li>
-                <li>Make an appointment with Tuen Mun Environmental Park WEEE·PARK for free on-site recycling.</li>
-            </ol>
-        }, {
-            type: "Fluorescent Lamps and Tubes",
-            img: ["/typeimage/Lamps.jpg","/typeimage/tube.jpg"],
-            guideline:
-            <ol>
-                <li>Before placing used lamps in the collection box for recycling, place them in the packaging that comes with new lamps.</li>
-                <li>Broken lamps should be sealed in a plastic bag.</li>
-            </ol>
-        }, {
-            type: "Rechargeable Batteries",
-            img: ["/typeimage/battery.jpg", "/typeimage/mobile.jpg","/typeimage/powerbank.jpg"],
-            guideline:
-            <ol>
-                <li>Remove the rechargeable battery from the equipment.</li>
-                <li>Cover the battery terminal with masking tape.</li>
-                <li>Do not put damaged rechargeable batteries into the collection boxes.</li>
-                <li> For batteries that are vulnerable to damage, put them in plastic bag and seal them with adhesive tape before deposit.</li>
-                <li>Bring the battery to a Collection Point for recycling.</li>
-            </ol>
-        }, {
-            type: "Tetra Pak",
-            img: ["/typeimage/pak.jpg","/typeimage/tea.jpg"],
-            guideline:
-            <ol>
-                <li>Removing straw, sleeve and cap. </li>
-                <li>Rinsing and cleaning the beverage carton.</li>
-                <li>Flattening the beverage carton.</li>
-                <li>Bring tetra pak to Collection Point for recycling.</li>
-            </ol>
-        }, {
-            type: "Clothes",
-            img: ["/typeimage/clothes.jpg","/typeimage/637808029647630000.jpg","/typeimage/shoes.jpg"],
-            guideline:
-            <ol>
-                <li>Clean the items before recycling</li>
-            </ol>
-        }, {
-            type: "Furniture",
-            img: ["/typeimage/table.jpg","/typeimage/so.jpg","/typeimage/office.jpg"],
-            guideline:
-            <ol>
-                <li>Relatively new and clean on the outside</li>
-                <li>Reusable</li>
-                <li>Structurally safe</li>
-            </ol>
-        }, {
-            type: "Food",
-            img: ["/typeimage/food.jpg","/typeimage/food2.jpg"],
-            guideline:
-            <ol>
-                <li>2 weeks or more before expiry date.</li>
-                <li>Dry food and drinks with intact package.</li>
-                <li>Fresh food (fruits and vegetables)</li>
-                <li>Oils & seasonings</li>
-                <li>Frozen/ chilled food</li>
-                <li>Bakery items </li>
-                <li>Cooked food</li>
-            </ol>
-        }, {
-            type: "Others",
-            img: ["/typeimage/foodwaste.jpg","/typeimage/wood.jpg","/typeimage/banner.jpg","/typeimage/tyre.jpg"],
-            guideline:
-            <ol>
-                <li>Toner Cartridge</li>
-                <li>Wood</li>
-                <li>PVC Banner</li>
-                <li>Rubber Tyre</li>
-                <li>Food Waste</li>
-                <li>Restaurant Waste (Oil, Grease Trap)</li>
-            </ol>
-        }
-    ]
+    const items = genItem()
 
 
     const genTable = () => {
@@ -154,7 +28,7 @@ const TypePage = () => {
                 <tr>
                     <th scope="col">Material Type</th>
                     <th scope="col">Recyclable Materials</th>
-                    <th scope="col" >Guideline</th>
+                    <th scope="col">Guideline</th>
                 </tr>
                 </thead>
                 {genTable()}
